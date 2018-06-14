9f8a0fc4eb7704a279eebe147cddfc6c40c0527c
DESKTOP-420EADS\gvagi <g.vagias@dotbydot.gr>
2018-06-14 17:50:55 +0300

    add IAudioManager interface

---
+++ src/Panacea.Controls/IAudioManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panacea.Controls
+{
+    public interface IAudioManager
+    {
+
+        void MaxDevicesExceptDefault();
+
+        /// <summary>
+        /// Set speakers volume. Minimum 0, maximum 1
+        /// </summary>
+        float SpeakersVolume { get; set; }
+
+        /// <summary>
+        /// Set microphones volume. Minimum 0, maximum 1
+        /// </summary>
+        float MicrophoneVolume{ get; set; }
+    }
+}
