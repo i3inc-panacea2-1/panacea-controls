ca51ae5178c9481687e9df12e9198626c439efe9
DESKTOP-420EADS\gvagi <g.vagias@dotbydot.gr>
2018-06-15 11:52:41 +0300

    add AudioManager class

---
+++ src/Panacea.Controls/AudioManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panacea.Controls
+{
+    public class AudioManager : IAudioManager
+    {
+        public float SpeakersVolume { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public float MicrophoneVolume { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void MaxDevicesExceptDefault()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
