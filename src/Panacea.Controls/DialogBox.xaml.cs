d4a17888bc4e745de5c77676a3323bbd05cd0a74
DESKTOP-420EADS\gvagi <g.vagias@dotbydot.gr>
2018-06-26 17:05:41 +0300

    DialogBox control

---
+++ src/Panacea.Controls/DialogBox.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Panacea.Controls
+{
+    /// <summary>
+    /// Interaction logic for DialogBox.xaml
+    /// </summary>
+    public partial class DialogBox : Window
+    {
+        public DialogBox(string text, Window owner)
+        {
+            InitializeComponent();
+            
+            SizeToContent = SizeToContent.WidthAndHeight;
+            Text = text;
+            Owner = owner;
+            WindowStartupLocation = WindowStartupLocation.CenterOwner;
+        }
+
+
+
+
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(DialogBox), new PropertyMetadata(null));
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
