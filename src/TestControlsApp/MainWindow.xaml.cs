883ff9efb84b4def00b53176dbc60364df0f7abe
DESKTOP-420EADS\gvagi <g.vagias@dotbydot.gr>
2018-06-26 17:35:18 +0300

    test wpf application for development

---
+++ src/TestControlsApp/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Panacea.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestControlsApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Toast_Click(object sender, RoutedEventArgs e)
+        {
+            ToastNotification not = new ToastNotification("test message", this);
+            //not.Opacity = 0;
+            not.Show();
+        }
+
+        private void Dialog_Click(object sender, RoutedEventArgs e)
+        {
+            DialogBox dlg = new DialogBox("Test Messsage", this);
+            dlg.Show();
+        }
+    }
+}
