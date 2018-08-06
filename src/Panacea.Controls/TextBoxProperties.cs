f8ce5adc3094cd40eb69a260955f807a5843ac2f
Portno <j.remoundos@dotbydot.gr>
2018-08-06 16:33:00 +0300

    more textbox styles

---
+++ src/Panacea.Controls/TextBoxProperties.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Panacea.Controls
+{
+    public class TextboxProperties
+    {
+        public static string GetLabel(DependencyObject obj)
+        {
+            return (string)obj.GetValue(LabelProperty);
+        }
+
+        public static void SetLabel(DependencyObject obj, string value)
+        {
+            obj.SetValue(LabelProperty, value);
+        }
+
+        public static readonly DependencyProperty LabelProperty =
+            DependencyProperty.RegisterAttached(
+                "Label",
+                typeof(string),
+                typeof(TextboxProperties),
+                new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.Inherits));
+    }
+
+    public class MultiplyConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            double result = 1.0;
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (values[i] is double)
+                    result *= (double)values[i];
+            }
+
+            return result - 5;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new Exception("Not implemented");
+        }
+    }
+
+}
