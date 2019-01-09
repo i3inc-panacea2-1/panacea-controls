6f215b987d46b4b87c8f13c2071a35ffd3de87bd
Portno <j.remoundos@dotbydot.gr>
2019-01-09 15:01:14 +0200

    added card control

--- src/TestControlsApp/MainWindow.xaml.cs
+++ src/TestControlsApp/MainWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace TestControlsApp
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : PanaceaWindow
+    public partial class MainWindow : Window
     {
 
 
