97d86e66f73307da9877a4c86607cfef233cfb7b
Portno <j.remoundos@dotbydot.gr>
2019-01-10 12:58:56 +0200

    added ListView

---
+++ src/TestControlsApp/SampleListViewItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestControlsApp
+{
+    public class SampleListViewItem
+    {
+        public string Key { get; set; }
+
+        public string Value { get; set; }
+    }
+}
