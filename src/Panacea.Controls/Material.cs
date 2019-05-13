90cfee20cc596f57a3f2f3b2db26c9483ae6f2cc
Portno <j.remoundos@dotbydot.gr>
2019-05-13 08:51:07 +0300

    fix

--- src/Panacea.Controls/Material.cs
+++ src/Panacea.Controls/Material.cs
@@ -20,7 +20,7 @@ namespace Panacea.Controls
     {
         static Material()
         {
-            TextElement.FontSizeProperty.OverrideMetadata(typeof(TextElement), new FrameworkPropertyMetadata(OnFontSizeChanged));
+            TextBlock.FontSizeProperty.OverrideMetadata(typeof(TextBlock), new FrameworkPropertyMetadata(OnFontSizeChanged));
         }
 
         private static void OnFontSizeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
