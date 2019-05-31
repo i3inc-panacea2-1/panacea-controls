7c081c92d82a61a98a37724d6406acc6381ca3fa
Kostas Aronis <k.aronis@dotbydot.gr>
2019-05-31 16:04:34 +0300

    Create BoolToColorConverter.cs

---
+++ src/Panacea.Controls/Converters/BoolToColorConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows;
+
+namespace Panacea.Controls.Converters
+{
+    class BoolToColorConverter : IMultiValueConverter
+    {
+        public object Convert(object[] value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((ICommand)value[0]).CanExecute(value[1]) == true ? Application.Current.FindResource("ColorError") : Application.Current.FindResource("ColorInformation");
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
