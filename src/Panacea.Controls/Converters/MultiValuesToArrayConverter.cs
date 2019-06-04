b364a9bcb332ff20e9cafc36a96070f4e5bf87ff
Kostas Aronis <k.aronis@dotbydot.gr>
2019-06-04 11:56:31 +0300

    Added MultiValuesToArrayConverter

---
+++ src/Panacea.Controls/Converters/MultiValuesToArrayConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Panacea.Controls.Converters
+{
+    public class MultiValuesToArrayConverter : IMultiValueConverter
+    {
+        public object Convert(object[] value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var list = new object[value.Length];
+            for (var i = 0; i < value.Length; i++)
+            {
+                list[i] = value[i];
+            }
+            return list;
+        }
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
